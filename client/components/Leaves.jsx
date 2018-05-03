417b0fc9c3e032f21414c0b41988fccc9b382645
Maia <maiamiller25@gmail.com>
2018-05-03 16:01:41 +1200

    Leaf blows away, no rotation

--- client/components/Leaves.jsx
+++ client/components/Leaves.jsx
@@ -4,7 +4,6 @@ export default class Leaves extends React.Component {
   constructor(props) {
     super (props)
     this.state = {
-      //state
       visible: true
     }
     this.handleClick = this.handleClick.bind(this)
