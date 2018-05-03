b612bf6ad32dbbccdbe411e7baed0349fafe1848
Usha <usha@Ushas-MacBook-Air.local>
2018-05-03 17:15:12 +1200

    added sidebar

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import Columns from './Columns'
+import Sidebar from './Sidebar'
 
 const App = () => (
     <div className='app-container app-body'>
       <Columns />
+      <Sidebar />
     </div>
 )
 
