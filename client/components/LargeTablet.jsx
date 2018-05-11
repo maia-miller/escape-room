a39c68889854191fd79eafed596959f33cf60b09
Maia <maiamiller25@gmail.com>
2018-05-11 14:43:22 +1200

    LargeTablet opens in Modal

--- client/components/LargeTablet.jsx
+++ client/components/LargeTablet.jsx
@@ -1,10 +1,74 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
+import { connect } from 'react-redux'
 
-export default (props) => {
+import {toggleLargeTablet} from '../actions/largetablet.js'
 
-  return(
-    <div>
-      <img src='../../images/largetablet.png' />
-    </div>
-  )
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)',
+    overflow              : 'scroll',
+    height                : '70%',
+    background            : 'beige'
+  }
+};
+
+Modal.setAppElement('#app');
+
+class LargeTablet extends React.Component {
+  constructor(props) {
+    super (props)
+    this.state = {
+      modalIsOpen: false
+    };
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.props.dispatch(toggleLargeTablet())
+  }
+
+  render() {
+    return (
+      <div className='modal'>
+        <Modal
+          isOpen={this.props.largetablet}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Large Tablet"
+        >
+          <img src='../../images/largetablet.png' />
+
+        </Modal>
+      </div>
+    );
+  }
 }
+
+const mapStateToProps = (state) => {
+  console.log('state', state)
+  return {
+    leaves: state.leaves,
+    smalltabletfound: state.smalltabletfound,
+    largetablet: state.largetablet
+  }
+}
+
+export default connect(mapStateToProps)(LargeTablet)
