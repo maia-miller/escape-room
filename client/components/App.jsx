6021027ff9b8bbbe8f11d4b73eb0fd8c2a9fa502
Maia <maiamiller25@gmail.com>
2018-05-15 15:47:35 +1200

    Music autoplays on homescreen

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -19,6 +19,10 @@ render() {
   return(
     <Router>
       <div className='bg'>
+        <audio controls autoPlay>
+          <source src="../../images/bensound-theduel.mp3" />
+        </audio>
+
         <Route exact path='/LevelOne' component={LevelOne} />
 
         <Pulse duration={4} infinite>
@@ -27,29 +31,50 @@ render() {
               <img className='ping levelone' src='../../images/ping-active.png' />
             </Link>
 
-            <Link to='/LevelTwo'>
-              <img className='ping leveltwo' src='../../images/ping-inactive.png' />
-            </Link>
+            <div>
+              {this.props.levelone
+                ? <Link to='/LevelTwo'><img className='ping leveltwo' src='../../images/ping-active.png' /></Link>
+                : <Link to='/LevelTwo'><img className='ping leveltwo' src='../../images/ping-inactive.png' /></Link>
+              }
+            </div>
 
-            <Link to='/LevelThree'>
-              <img className='ping levelthree' src='../../images/ping-inactive.png' />
-            </Link>
+            <div>
+              {this.props.leveltwo
+                ? <Link to='/LevelThree'><img className='ping levelthree' src='../../images/ping-inactive.png' /></Link>
+                : <Link to='/LevelTwo'><img className='ping levelthree' src='../../images/ping-inactive.png' /></Link>
+              }
+            </div>
 
-            <Link to='/LevelFour'>
-              <img className='ping levelfour' src='../../images/ping-inactive.png' />
-            </Link>
+            <div>
+              {this.props.levelthree
+                ? <Link to='/LevelFour'><img className='ping levelfour' src='../../images/ping-inactive.png' /></Link>
+                : <Link to='/LevelTwo'><img className='ping levelfour' src='../../images/ping-inactive.png' /></Link>
+              }
+            </div>
 
-            <Link to='/LevelFive'>
-              <img className='ping levelfive' src='../../images/ping-inactive.png' />
-            </Link>
+            <div>
+              {this.props.levelfour
+                ? <Link to='/LevelFive'><img className='ping levelfive' src='../../images/ping-inactive.png' /></Link>
+                : <Link to='/LevelTwo'><img className='ping levelfive' src='../../images/ping-inactive.png' /></Link>
+              }
+        </div>
 
-            <Link to='/LevelSix'>
-              <img className='ping levelsix' src='../../images/ping-inactive.png' />
-            </Link>
+            <div>
+              {this.props.levelfive
+                ? <Link to='/LevelSix'><img className='ping levelsix' src='../../images/ping-inactive.png' /></Link>
+                : <Link to='/LevelTwo'><img className='ping levelsix' src='../../images/ping-inactive.png' /></Link>
+              }
+        </div>
+
 
           </div>
 
         </Pulse>
+
+        <div className='credit'>
+          Music: <a href='https://www.bensound.com'>https://www.bensound.com</a>
+        </div>
+
       </div>
     </Router>
         )
