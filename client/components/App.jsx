115200bdb93eb880c95b4be9bf2dcb6c3967992a
Maia <maiamiller25@gmail.com>
2018-05-15 14:06:24 +1200

    Background of App.jsx set to map, working on ping icon

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import {HashRouter as Router, Route} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 import LevelOne from './LevelOne'
 
-class App extends React.Component {
+export default class App extends React.Component {
   constructor(props) {
     super (props)
     this.state = {
@@ -14,13 +16,16 @@ class App extends React.Component {
 
 render() {
   return(
-    <div className='app-container'>
-      <LevelOne />
-    </div>
+    <Router>
+      <div className='bg'>
+        <Route exact path='/LevelOne' component={LevelOne} />
+        <div className='ping'>
+          <Link to='/LevelOne'>
+          <img className='ping' src='../../images/ping-active.png' />
+          </Link>
+        </div>
+      </div>
+    </Router>
         )
       }
     }
-
-
-
-export default connect()(App)
