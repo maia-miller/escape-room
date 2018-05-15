7f2267659fc416c3eab8fdb6d7503269ba9da648
Maia <maiamiller25@gmail.com>
2018-05-15 15:03:38 +1200

    6 level pings on map, pulsing together

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import {HashRouter as Router, Route} from 'react-router-dom'
 import {Link} from 'react-router-dom'
+import { Pulse } from 'react-motions'
 
 import LevelOne from './LevelOne'
 
@@ -19,11 +20,36 @@ render() {
     <Router>
       <div className='bg'>
         <Route exact path='/LevelOne' component={LevelOne} />
-        <div className='ping'>
-          <Link to='/LevelOne'>
-          <img className='ping' src='../../images/ping-active.png' />
-          </Link>
-        </div>
+
+        <Pulse duration={4} infinite>
+          <div className='ping'>
+            <Link to='/LevelOne'>
+              <img className='ping levelone' src='../../images/ping-active.png' />
+            </Link>
+
+            <Link to='/LevelTwo'>
+              <img className='ping leveltwo' src='../../images/ping-inactive.png' />
+            </Link>
+
+            <Link to='/LevelThree'>
+              <img className='ping levelthree' src='../../images/ping-inactive.png' />
+            </Link>
+
+            <Link to='/LevelFour'>
+              <img className='ping levelfour' src='../../images/ping-inactive.png' />
+            </Link>
+
+            <Link to='/LevelFive'>
+              <img className='ping levelfive' src='../../images/ping-inactive.png' />
+            </Link>
+
+            <Link to='/LevelSix'>
+              <img className='ping levelsix' src='../../images/ping-inactive.png' />
+            </Link>
+
+          </div>
+
+        </Pulse>
       </div>
     </Router>
         )
